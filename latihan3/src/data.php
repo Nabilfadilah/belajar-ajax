d938866a5545480aa844249ac7d23498d380d7b0
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-25 16:20:18 +0700

    Memberikan response dalam bentuk html pada php

--- latihan3/src/data.php
+++ latihan3/src/data.php
@@ -1,4 +1,11 @@
 <?php
+// kita bisa olah tau uji datanya
+
+// menyambungkan datanya ke database dengan query
+// select * from users where name = $_GET['keyword'];
+
+// memberikan response dalam bentuk html
+echo "Nama: <strong>" . $_GET['keyword'] . "</strong>";
 
 // terima data keywordnya
-echo $_GET['keyword'];
+// echo $_GET['keyword'];
